bfd6d359ef92c245bd34ec99787221d6a081424f
ziaulhaq-del <141904789+ziaulhaq-del@users.noreply.github.com>
2023-11-29 11:48:28 +0530

    Update and rename JfrogArtif.groovy to jfrog.py

--- vars/JfrogArtif.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-    echo '----------executing jfrog'
-    sh """ curl -X PUT -u admin:Admin123 -T /var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.82.162.232:8082
-}
---
+++ vars/jfrog.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import requests
+import subprocess
+
+def jfrogUpload():
+    url = "http://65.0.179.169:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+    file_path = "/var/lib/jenkins/workspace/NewPipeline/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar"
+    username = 'admin'
+    password = 'zia123'
+
+        #   send the PUT request with authentication and file upload
+    with open(file_path,'rb') as file:
+        response = requests.put(url, auth=(username, password), data=file)
+        # check the response status code
+    if response.status_code == 201:
+        print("\nPUT request was successful!")
+    else:
+        print(f"PUT reuquest failed with status code(response.status_code)")
+        print("Response content:")
+        print(response.text)
+
+def mvnBuild():
+   # Define the Maven command
+   maven_command = "mvn clean install -DskipTests"
+
+   # Run the Maven command as a subprocess
+
+try:
+    subprocess.run(maven_command, check=True, test=True, shell=True)
+    print("\nMaven build completed succesfully.")
+except subprocess.CalledProcessError as e:
+       print(f"Error: Maven build failed with exit code (e.returncode)")
+
+def main():
+    mvnBuild()
+    jfrogUpload()
+
+if __name__=="__main__":
+    main()
